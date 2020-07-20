06f9f6f588b85279089cb092ddfb3eed9e4da56f
Andy <gl_andy@mail.ru>
2020-07-21 01:10:36 +0500

    Создал модуль для обращения к серверу

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 'use strict';
 (function () {
+  var onError = function (message) {
+    console.log(message);
+  };
+  var onSuccess = function (data) {
+    console.log(data);
+  };
+  window.backend.load('https://javascript.pages.academy/keksobooking/data', onSuccess, onError);
+
   window.addEventListener('load', function () {
     window.map.disableMap();
   });
